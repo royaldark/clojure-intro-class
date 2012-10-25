f2bf655079fb5bb2b748aaefbf5ccc57b930161a
Joe Einertson <eine0017@morris.umn.edu>
2012-10-25 01:53:12 -0500

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject intro "UMM Intro Clojure"
+ :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
+ :dependencies [[org.clojure/clojure "1.5.0-alpha7"]]
+ :main intro.core)
