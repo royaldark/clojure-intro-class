a1480c6ac596814d66d32b5326596a979bab80ce
Joe Einertson <jeinerts@umm>
2012-10-29 02:01:09 -0500

    Added clj-stacktrace to list of dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject intro "UMM Intro Clojure"
  :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
- :dependencies [[org.clojure/clojure "1.5.0-alpha7"]]
+ :dependencies [[org.clojure/clojure "1.5.0-alpha7"]
+                [clj-stacktrace "0.2.5"]]
  :main intro.core)
