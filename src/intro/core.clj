19cca47eeed280e6bfd6f4a6037688096db5f80b
Joe Einertson <jeinerts@umm>
2012-10-30 19:23:29 -0500

    Added a basic Seesaw example to the demonstration

--- src/intro/core.clj
+++ src/intro/core.clj
@@ -1,8 +1,17 @@
 (ns intro.core
   (:require [errors.core :as errors])
-  (:use [corefns.core]))
+  (:use [corefns.core]
+        [seesaw.core]))
+
+(defn basic-seesaw-frame []
+  (invoke-later
+    (def f (frame :title "Hello",
+                  :content (button :text "Seesaw Rocks!"),
+                  :on-close :exit))
+    (-> f pack! show!)))  
 
 (defn -main [& args]
   (try
+    (basic-seesaw-frame)
     (map 2 [1 2 3])
     (catch Throwable e (println (errors/prettify-exception e)))))
