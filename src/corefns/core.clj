c06e25edd6762f339756d328d5ec7ac68cb36334
Joe Einertson <eine0017@kevlar.morris.umn.edu>
2012-10-31 15:05:11 -0500

    Made assertion errors more readable

--- src/corefns/core.clj
+++ src/corefns/core.clj
@@ -1,6 +1,12 @@
 (ns corefns.core
 (:refer-clojure :exclude [map]))
 
-(defn map [f coll]
-  {:pre [(coll? coll) (fn? f)]}
-  (clojure.core/map f coll))
+(defn is-function? [x]
+(fn? x))
+
+(defn is-collection? [x]
+(coll? x))
+
+(defn map [argument1 argument2]
+  {:pre [(is-collection? argument2) (is-function? argument1)]}
+  (clojure.core/map argument1 argument2))
