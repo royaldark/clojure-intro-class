e4709b19cf9455235bfca8532b1e36e517ce03b6
Joe Einertson <jeinerts@umm>
2012-11-28 03:33:23 -0600

    Added support for non-GUI friendly error messages

--- src/errors/core.clj
+++ src/errors/core.clj
@@ -8,15 +8,23 @@
   (if-let [entry (some #(when (instance? (:class %) e) %) error-dictionary)]
     (if-let [pretty-message (clojure.string/replace (.getMessage e) (:match entry) (:replace entry))]
       pretty-message
-    (.getMessage e))
-  (.getMessage e)))
+      (.getMessage e))
+    (.getMessage e)))
 
 (defn- show-error [msg]
-  (invoke-later
-    (native!)
-    (let [d (dialog :title "Clojure Error",
-                   :content (text :multi-line? true :editable? false :text msg))]
-      (-> d pack! show!))))
+  (try
+    (invoke-now
+      (native!)
+      (let [d (dialog :title "Clojure Error",
+                      :content (text :multi-line? true :editable? false :text msg))]
+        (-> d pack! show!)))
+    (catch java.lang.reflect.InvocationTargetException e
+      (if (instance? java.awt.HeadlessException (.getCause e))
+        ; If there is no GUI available, this throws an InvocationTargetException
+        ; wrapping a HeadlessException - print the error instead of showing a window.
+        (println msg)
+        ; And if the error does not originate from a HeadlessException, throw it again.
+        (throw e)))))
 
 (defn prettify-exception [e]
   (let [info (stacktrace/parse-exception e)
