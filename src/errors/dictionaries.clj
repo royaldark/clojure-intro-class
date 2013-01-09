c5f6a95718dd9e8166e41eb0a011af6de6dd1517
Elena M <elenam@morris.umn.edu>
2013-01-08 22:44:40 -0600

    Started on function type matching. It seems to work in repl, but doesn't work here for some reason.

--- src/errors/dictionaries.clj
+++ src/errors/dictionaries.clj
@@ -8,7 +8,14 @@
 ;; A string representation of a type t not listed in the type-dictionary
 (defn unknown-type-string [t]
   "returns a string representation of a type t not listed in the type-dictionary for user-friendly error messages"
-  (str "unknown type " t))
+  ;; functions
+  ;; a code pattern: (instance? (resolve (symbol t)) 5)
+  ;; (if (instance? clojure.lang.IFn (resolve (symbol t))) "function"
+  (if (ifn? (resolve (symbol t))) "function" ;; fails for some reason; shouldn't
+  ;; collections - might want to move to earlier since some seqs implement the IFn interface
+
+  ;; if all else fails: 
+  (str "unrecognized type " t)))
 
 ;; The best approximation of a type we can get if it's not listed in the type-dictionary
 (defn other-type [t]
