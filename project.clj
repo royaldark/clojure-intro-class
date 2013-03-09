d2dd5761521bdf3bf6f4efd191b977eb6eef114b
Joe Einertson <jeinerts@umm>
2013-03-09 23:53:04 +0100

    Updated dependencies (Clojure 1.5, Seesaw 1.4.3)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject intro "UMM Intro Clojure"
             :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
-            :dependencies [[org.clojure/clojure "1.5.0-beta1"]
+            :dependencies [[org.clojure/clojure "1.5.0"]
                            [clj-stacktrace "0.2.5"]
-                           [seesaw "1.4.2"]]
+                           [seesaw "1.4.3"]]
             :main intro.core)
