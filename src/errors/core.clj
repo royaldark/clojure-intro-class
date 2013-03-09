341817ffbe2c844a67c92f8c1479893fb1903ba3
Joe Einertson <jeinerts@umm>
2013-03-10 00:48:49 +0100

    Added tab with original stacktrace in error window

--- src/errors/core.clj
+++ src/errors/core.clj
@@ -18,14 +18,33 @@
       message)
     message)))
 
+(defn format-stacktrace [e]
+  ; Force Java to give us the preformatted stacktrace as a string
+  (let [writer (java.io.StringWriter.)]
+    (.printStackTrace e (java.io.PrintWriter. writer))
+    (.toString writer)))
+
 ;; Graphics 
-(defn- show-error [msg]
+(defn- show-error [msg e]
   (try
     (invoke-now
       (native!)
-      (let [d (dialog :title "Clojure Error",
-                      :content (text :multi-line? true :editable? false :text msg))]
+      (let [errormsg (text :multi-line? true :editable? false :text msg)
+            stacktrace (text :multi-line? true :editable? false :rows 12 :text (format-stacktrace e))
+            d (dialog :title "Clojure Error",
+                      :content (tabbed-panel :placement :bottom
+                                             :overflow :scroll
+                                             :tabs [{:title "Error"
+                                                     :tip "The simplified error message"
+                                                     :content (scrollable errormsg)}
+                                                    {:title "Stacktrace"
+                                                     :tip "The full Java stacktrace of the error"
+                                                     :content (scrollable stacktrace)}])
+                      )]
+        (scroll! errormsg :to :top)   ;; Scrollboxes default to being scrolled to the bottom - not what we want
+        (scroll! stacktrace :to :top)
         (-> d pack! show!))) ;; adding request-focus! here doesn't work -- Elena
+                             ;; request-focus! asks for focus to a certain item WITHIN a window, not focus for an entire window -- Joe
     (catch java.lang.reflect.InvocationTargetException e
       (if (instance? java.awt.HeadlessException (.getCause e))
         ; If there is no GUI available, this throws an InvocationTargetException
@@ -40,4 +59,6 @@
         cljerrs (filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %))))
                         (:trace-elems info))
         errstrs (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")") cljerrs)]
-    (show-error (str "ERROR: " (get-pretty-message e) "\nPossible causes:\n" (join "\n" errstrs)))))
+    (show-error
+      (str "ERROR: " (get-pretty-message e) "\nPossible causes:\n" (join "\n" errstrs))
+      e)))
