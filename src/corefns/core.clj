d92002064e73437f397913c0173c9c371c607482
Elena M <elenam@morris.umn.edu>
2013-03-16 10:42:12 -0500

    Added add-first and add-last functions

--- src/corefns/core.clj
+++ src/corefns/core.clj
@@ -1,11 +1,22 @@
 (ns corefns.core
-(:refer-clojure :exclude [map filter nth]))
+  (:refer-clojure :exclude [map filter nth]))
 
-; A few function aliases to increase the readability of
-; error messages caused by failed assertions
+;; A few function aliases to increase the readability of
+;; error messages caused by failed assertions
 (def is-function? fn?)
 (def is-collection? coll?)
 (def is-number? number?)
+(def is-vector-or-list? #(or (vector? %) (list? %)))
+
+;; Functions for type-independent sequence handling
+;; Need to define predicates on them as well
+(defn add-first [argument1 argument2]
+  {:pre [(is-collection? argument1)]}
+  (cons argument2 argument1))
+
+(defn add-last [argument1 argument2]
+  {:pre [(is-collection? argument1)]}
+  (concat coll [x]))
 
 ;; filter and map have the same checks. Should we abstract over this?
 ;; note that for filter the function must return a boolean, but there
@@ -20,7 +31,7 @@
   (clojure.core/filter argument1 argument2))
 
 (defn nth [argument1 argument2]
-  {:pre [(is-collection? argument1) (is-number? argument2)]}
+  {:pre [(is-vector-or-list? argument1) (is-number? argument2)]}
    (clojure.core/nth  argument1 argument2))
 
 ;; need conj, into. Careful: there may be different cases. Can we have a complex pre-cond (with cases)?
