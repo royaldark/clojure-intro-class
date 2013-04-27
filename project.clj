e04c0eb7d7456a9012c941bc53db4186cb2f0c79
Stephen A <StephenAdams.Minn@gmail.com>
2013-04-27 14:52:45 -0500

    Added properly formatted version number to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject intro "UMM Intro Clojure"
+(defproject intro "0.1"
             :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
             :dependencies [[org.clojure/clojure "1.5.0"]
 			   [clj-stacktrace "0.2.5"]
