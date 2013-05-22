f4171ce3c3a4a1ab95247bb5c4d0b1271268b2c0
Elena M <elenam@morris.umn.edu>
2013-05-22 10:26:15 -0500

    commented out attempts at getting contracts to work, saved for history. Added a to-do list.

--- src/corefns/core.clj
+++ src/corefns/core.clj
@@ -2,9 +2,16 @@
   (:use [clojure.core.incubator])
   (:require [trammel.provide :as provide]
     	    [trammel.core :as tramm]
-    	    [clojure.core.contracts :as contracts]))
-  ;(:refer-clojure :exclude [map filter nth]));[map filter nth concat]))
+    	    [clojure.core.contracts :as contracts])
+  (:refer-clojure :exclude [map]));[map filter nth concat]))
 
+;; to-do:
+;; 1. remove references to contracts, trammel (from project.clj as well)
+;; 2. modify our type-checking functions to record the type (or the arg? or the message?)
+;; 3. modify error--handling for asserts to check the recorded info
+;; 4. rewrite messages similar to the standard ones (perhaps abstract over?)
+;; 5. don't forget to clear the queue at the end (post-cond? or the end of pre-cond? or finally?)
+;; 6. why didn't I think of this earlier? 
 
 ;; A few function aliases to increase the readability of
 ;; error messages caused by failed assertions
@@ -23,9 +30,9 @@
 ;; note that for filter the function must return a boolean, but there
 ;; is no way to check for it, is there? - Elena
 
-;(defn map [argument1 argument2]
-;  {:pre [(is-collection? argument2) (is-function? argument1)]}
-;  (clojure.core/map argument1 argument2))
+(defn map [argument1 argument2]
+  {:pre [(do (println argument2) true) (is-collection? argument2) (is-function? argument1)]}
+  (clojure.core/map argument1 argument2))
 
 ;(defn filter [argument1 argument2]
 ;  {:pre [(is-collection? argument2) (is-function? argument1)]}
@@ -45,22 +52,51 @@
 
 ;; need conj, into. Careful: there may be different cases. Can we have a complex pre-cond (with cases)?
 
+(comment
 (tramm/defcontract second-arg-sequence
   		"Second argument must be a sequence"
-     		[f coll] [(do (println (class coll)) true) (seqable? coll)])
+     		[f coll] ;(do (println (class coll)) true) 
+     			[(seqable? coll)])
 
 (tramm/defcontract first-arg-function
   		"First argument must be a function"
-     		[f _] [(is-function? f)])
+     		[f coll] [(do (println f " " coll) true) 
+     			(fn? f)])
 
-;; testing contracts
-(def map 
-	(contracts/with-constraints 
-		map 
-		second-arg-sequence
-		first-arg-function))
+(tramm/defcontract
+  doubler-defcontract
+  "Test"
+  [n] [(do (println (class n)) true) (number? n) => (= % (* 2 n))]
+  [x y] [(do (println (class x) " " (class y)) true) (every? number? [x y]) ])
+
+(println (= 10 ((partial doubler-defcontract #(* 2 (+ %1 %2))) 3 3)))
+
+(def testf (partial doubler-defcontract #(* 2 (+ %1 %2))))
+
+(println (testf 2 3))
+
+(tramm/defcontract is-function
+  		"First argument must be a function"
+     		[f] [(do (println (class f)) true) 
+     			(fn? f)])
+
+
+;(def myf (partial is-function #(%1 %2)))
+
+;(println ((myf dec) 2))
+
+;; This doesn't quite do what we want since map can take a variable number of arguments.
+;; Plus we don't want to overwrite map, we want to add constraints to the existing map
+;; if we can. 
+(def mymymap (partial second-arg-sequence #(println %1 %2)))
+	;(contracts/with-constraints
+	;	map 
+		;second-arg-sequence
+	;	first-arg-function
+	;	))
 		;map))
-
+)
+		
 ;; Functions for type-independent sequence handling
 ;; Need to define predicates on them as well
 (defn add-first [argument1 argument2]
