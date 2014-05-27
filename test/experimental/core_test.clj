27b1912fc141f44c3d84a092d534b87627017af7
Henry Fellows <Fello056@morris.umn.edu>
2014-05-27 14:08:12 -0500

    expanded tests, created exceptionobj.clj
    created our carrier-class, and built more tests.

--- test/experimental/core_test.clj
+++ test/experimental/core_test.clj
@@ -33,70 +33,17 @@
              (catch Exception e e)))
 
 ;a helper function to cleanly test the above.
-(defn- exeception->string
-  "Converts execeptions to strings, returning a string or the original "
+(defn- exception->string
+  "Converts exceptions to strings, returning a string or the original e (if it is not an exception)"
   [e] (if (instance? Exception e)
                                 (.getMessage e)
                                 e))
 
 (expect "java.lang.Long cannot be cast to clojure.lang.IFn"
-        (exeception->string (run-and-catch '(1 3))))
+        (exception->string (run-and-catch '(1 3))))
 
 (expect 3
-        (exeception->string (run-and-catch '(+ 1 2))))
-
-;*************************************
-;*** Using the above with errors.core ***.
-
-
-;!!!!DANGER!!!!
-;if opened in a repl, it will launch infinite windows.
-
-;(comment
-(expect :success
-        (prettify-exception (run-and-catch '(2))))
-
-;  )
-
-;holy ****!
-;that's annoying.
-;we really need a version of prettify-exception that's not throwing windows at my face.
-
-;temporary workspace
-;*************************************
-;Testing the message object stuff.
-
-
-;Tests for make-msg-preobj-hash
-
-(expect {:msg "Blue Jeans", :stylekey :reg, :length 10}
-        (make-msg-preobj-hash "Blue Jeans"))
-
-(expect {:msg "Bootcut", :stylekey :hippy, :length 7}
-        (make-msg-preobj-hash "Bootcut" :hippy))
-
-
-;Tests for make-preobj-hashes
-
-(expect [{:msg "Blue Jeans", :stylekey :reg, :length 10}
-         {:msg "Khakis", :stylekey :business-casual, :length 6}
-         {:msg "Bootcut", :stylekey :hippy, :length 7}
-         {:msg "Jeggings", :stylekey :casual, :length 8}]
-        (make-preobj-hashes "Blue Jeans"
-                            "Khakis" :business-casual
-                            "Bootcut" :hippy
-                            "Jeggings" :casual))
-
-;Tests for make-obj
-
-(expect [{:start 0, :msg "Blue Jeans", :stylekey :reg, :length 10}
-         {:start 10, :msg "Khakis", :stylekey :business-casual, :length 6}
-         {:start 16, :msg "Bootcut", :stylekey :hippy, :length 7}
-         {:start 23, :msg "Jeggings", :stylekey :casual, :length 8}]
-        (make-obj [{:msg "Blue Jeans", :stylekey :reg, :length 10}
-                   {:msg "Khakis", :stylekey :business-casual, :length 6}
-                   {:msg "Bootcut", :stylekey :hippy, :length 7}
-                   {:msg "Jeggings", :stylekey :casual, :length 8}]))
+        (exception->string (run-and-catch '(+ 1 2))))
 
 
 
