33dd4f768a8ba22092222a36273a6f6ae9748d6c
Henry Fellows <Fello056@morris.umn.edu>
2014-05-27 17:07:03 -0500

    Misc experiments with stacktraces

--- test/experimental/core_test.clj
+++ test/experimental/core_test.clj
@@ -1,4 +1,5 @@
 (ns experimental.core_test
+  (:require [clj-stacktrace.core :as stacktrace])
   (:require [expectations :refer :all]
             [errors.messageobj :refer :all]
             [errors.core :refer :all]))
@@ -45,6 +46,24 @@
 (expect 3
         (exception->string (run-and-catch '(+ 1 2))))
 
+;************************************
+
+(def st1 (stacktrace/parse-exception (run-and-catch '(1))))
+
+(last (stacktrace/parse-exception (run-and-catch '(1))))
+
+
+(def st2 (stacktrace/parse-exception (run-and-catch '(+ 2 "pie"))))
+
+(def st3 (stacktrace/parse-exception (run-and-catch '(5))))
+
+(def q 2)
+
+(def fst1 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st1))))
+
+(def fst2 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st2))))
+
+(def fst3 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st3))))
 
 
 
