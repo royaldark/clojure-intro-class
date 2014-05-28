b3ba6362e4ee1544efe0a7fc406f2fc9b1cf3438
Henry Fellows <Fello056@morris.umn.edu>
2014-05-28 11:36:00 -0500

    terrible attempts at serialization

--- test/experimental/core_test.clj
+++ test/experimental/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clj-stacktrace.core :as stacktrace])
   (:require [expectations :refer :all]
             [errors.messageobj :refer :all]
-            [errors.core :refer :all]))
+            [errors.core :refer :all]
+            [clojure.java.io :as io]))
 
 ;;;; A space for prototypes, examples, and experimental features.
 ;; NEVER refer to this file in other files.
@@ -47,31 +48,54 @@
         (exception->string (run-and-catch '(+ 1 2))))
 
 ;************************************
+(def ex1 (run-and-catch '(1)))
 
-(def st1 (stacktrace/parse-exception (run-and-catch '(1))))
+(def ex2 (run-and-catch '(+ 2 "pie")))
 
-(last (stacktrace/parse-exception (run-and-catch '(1))))
+(def ex3 (run-and-catch '(5)))
 
+(def st1 (stacktrace/parse-exception ex1))
 
-(def st2 (stacktrace/parse-exception (run-and-catch '(+ 2 "pie"))))
+(def st2 (stacktrace/parse-exception ex2))
 
-(def st3 (stacktrace/parse-exception (run-and-catch '(5))))
+(def st3 (stacktrace/parse-exception ex3))
 
-(def q 2)
+;(def fst1 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st1))))
 
-(def fst1 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st1))))
+;(def fst2 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st2))))
 
-(def fst2 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st2))))
-
-(def fst3 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st3))))
-
-
+;(def fst3 (map #(str "\t" (:ns %) "/" (:fn %) " (" (:file %) " line " (:line %) ")")(filter #(and (:clojure %) (not (re-matches ignore-nses (:ns %)))) (:trace-elems st3))))
 
 
+(defn serialize
+  "Writes an exception to a file, in a way that preserves the stacktrace's data"
+  [exception filename]
+  (with-out-writer
+    (java.io.File. filename)
+    (binding [*print-dup* true] (prn exception))))
 
+(defn write-file2 []
+  (with-open [w (clojure.java.io/writer  "f:/w.txt" :append true)]
+    (.write w (str "hello" "world"))))
 
+(defn deserialize [filename]
+  (with-open [r (PushbackReader. (FileReader. filename))]
+    (read r)))
 
+(serialize ex1 "test.exception")
 
+(instance? java.io.Serializable ex1)
+
+(defn write-file
+  [except file]
+
+  (spit file "" :append false)
+  (with-open [out-data (.writeObject except (io/writer file))]
+     (.write out-data file)))
+
+(write-file ex1 "foo.bar")
+
+(.writeObject ex1 (io/writer "foo.bark"))
 
 
 
