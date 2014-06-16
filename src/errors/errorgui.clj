5cbd9557d1082a783642546dc647d8c6a96b4334
elenam <elenam@morris.umn.edu>
2014-06-16 16:51:32 -0500

    fixed a variables in errorgui and refactored

--- src/errors/errorgui.clj
+++ src/errors/errorgui.clj
@@ -12,11 +12,11 @@
 
 ;; Formatting functions
 
-(defn format-stacktrace [e]
-  ; Force Java to give us the preformatted stacktrace as a string
-  (let [writer (java.io.StringWriter.)]
-    (.printStackTrace e (java.io.PrintWriter. writer))
-    (.toString writer)))
+;(defn format-stacktrace [e]
+; Force Java to give us the preformatted stacktrace as a string
+; (let [writer (java.io.StringWriter.)]
+;    (.printStackTrace e (java.io.PrintWriter. writer))
+;    (.toString writer)))
 
 (defn trace-elem->string [trace-elem]
   "Takes a stack trace element from a parsed exception
@@ -45,57 +45,55 @@
                             (:length %))
               msg-obj)))
 
-
+(defn- make-message-with-filtered-trace [exc-obj]
+  "returns a message object that includes the filtered stacktrace"
+  (make-obj (concat (make-preobj-hashes error-prefix :err)
+		    (:message-object exc-obj)
+		    (make-preobj-hashes trace-lines-separator) ; to separate the message from the stack trace
+		    (make-preobj-hashes (trace->string (:filtered-stacktrace exc-obj))
+					:stack))))
 
 ;; Graphics
 ;; msg-obj will contain parts and styles and lengths
 (defn display-error [exc-obj]
-  (let [msg-obj  (:message-object exc-obj)]
-  (try
-    (let ;; styles for formatting various portions of a message
-	[
-	 trace (:stacktrace exc-obj)
-	 msg-filtered-trace (make-obj (concat (make-preobj-hashes error-prefix :err)
-					      msg-obj
-					      (make-preobj-hashes trace-lines-separator) ; to separate the message from the stack trace
-					      (make-preobj-hashes
-					      (trace->string (:filtered-stacktrace exc-obj)) :stack)))
-	 ;dummy (println msg-filtered-trace)
-	 styles [[:arg :font "monospace" :bold true] [:reg] [:stack] [:err] [:type] [:causes]]
-	 errormsg (styled-text :wrap-lines? true :text (str (get-all-text msg-filtered-trace))
-			       :styles styles)
-	 stacktrace (text :multi-line? true :editable? false :rows 12 :text 
-					(trace->string trace))
-			  ;"banana")
-	 d (dialog :title "Clojure Error",
-                 :content (tabbed-panel :placement :bottom
-                                        :overflow :scroll
-                                        :tabs [{:title "Error"
-                                                :tip "The simplified error message"
-                                                :content (do (display-msg-object! msg-filtered-trace errormsg)
-							     (scrollable errormsg))}
-									     
-                                               {:title "Stacktrace"
-                                                :tip "The full Java stacktrace of the error"
-                                                :content (scrollable stacktrace)}])
-                 )]
-      ;(println exc-obj) ; debug print
-      (invoke-now
-        (scroll! errormsg :to :top) ;; Scrollboxes default to being scrolled to the bottom - not what we want
-        (scroll! stacktrace :to :top)
-        (.setAlwaysOnTop d true) ;; to make errors pop up on top of other programs
-        ;; a mouse anywhere in the window resets it's always-on-top to false
-        (listen d :mouse-entered (fn [e] (.setAlwaysOnTop d false)))
-        (-> d pack! show!)))
-
-    (catch java.lang.reflect.InvocationTargetException e
-      (if (instance? java.awt.HeadlessException (.getCause e))
-        ; If there is no GUI available on Windows, this throws an InvocationTargetException
+  "Takes an exception object and displays it in a swing panel"
+  ;; extracting elements of exception-object and
+  ;; forming a message object with the filtered stack trace to be displayed
+  (let [trace (:stacktrace exc-obj)
+	msg-filtered-trace (make-message-with-filtered-trace exc-obj)]
+    (try
+      (let ;; styles for formatting various portions of a message
+	  [styles [[:arg :font "monospace" :bold true] [:reg] [:stack] [:err] [:type] [:causes]]
+	   ;; forming graphical elements of the swing panel 
+	   errormsg (styled-text :wrap-lines? true :text (str (get-all-text msg-filtered-trace))
+				 :styles styles)
+	   stacktrace (text :multi-line? true :editable? false :rows 12 :text 
+			    (trace->string trace))
+	   d (dialog :title "Clojure Error",
+		     :content (tabbed-panel :placement :bottom
+					    :overflow :scroll
+					    :tabs [{:title "Error"
+						    :tip "The simplified error message"
+						    :content (do (display-msg-object! msg-filtered-trace errormsg)
+								 (scrollable errormsg))}
+						   {:title "Stacktrace"
+						    :tip "The full Java stacktrace of the error"
+						    :content (scrollable stacktrace)}]))]
+	(invoke-now
+	 (scroll! errormsg :to :top) ;; Scrollboxes default to being scrolled to the bottom - not what we want
+	 (scroll! stacktrace :to :top)
+	 (.setAlwaysOnTop d true) ;; to make errors pop up on top of other programs
+	 ;; a mouse anywhere in the window resets it's always-on-top to false
+	 (listen d :mouse-entered (fn [e] (.setAlwaysOnTop d false)))
+	 (-> d pack! show!)))
+      (catch java.lang.reflect.InvocationTargetException e
+	(if (instance? java.awt.HeadlessException (.getCause e))
+	; If there is no GUI available on Windows, this throws an InvocationTargetException
         ; wrapping a HeadlessException - print the error instead of showing a window.
-        (println (get-all-text msg-obj)) ;; TODO: change this!!!!
-        ; And if the error does not originate from a HeadlessException, throw it again.
-        (throw e)))
-    (catch java.awt.HeadlessException e
+	  (println (get-all-text msg-filtered-trace)) 
+	; And if the error does not originate from a HeadlessException, throw it again.
+	  (throw e)))
+      (catch java.awt.HeadlessException e
       ; If there is no GUI available on Linux, it simply throws a HeadlessException - print the erorr.
-      (println (get-all-text msg-obj)))))) ;; TODO: change this!!!!
+	(println (get-all-text msg-filtered-trace)))))) 
 
